a616c348bf351c8384cac172b29eef166f0f66df
MightAsWell-5799 <sebhase@gmail.com>
2022-02-08 01:27:20 -0500

    NEED TO FIX THE FUCKING CALL STACK

--- wordle.js
+++ wordle.js
@@ -1,6 +1,80 @@
 var Answers = require("./puzzleWordsOut.json").words
+var issues = [
+	{ word: "shame", scorePos: 1 },
+	{ word: "shape", scorePos: 1 },
+	{ word: "swore", scorePos: 1 },
+	{ word: "sneer", scorePos: 1 },
+	{ word: "grave", scorePos: 1 },
+	{ word: "sweet", scorePos: 1 },
+	{ word: "steer", scorePos: 1 },
+	{ word: "graze", scorePos: 1 },
+	{ word: "sleep", scorePos: 1 },
+	{ word: "fried", scorePos: 1 },
+	{ word: "grove", scorePos: 1 },
+	{ word: "drove", scorePos: 1 },
+	{ word: "fable", scorePos: 1 },
+	{ word: "erode", scorePos: 1 },
+	{ word: "folly", scorePos: 1 },
+	{ word: "waste", scorePos: 1 },
+	{ word: "eagle", scorePos: 1 },
+	{ word: "water", scorePos: 1 },
+	{ word: "eater", scorePos: 1 },
+	{ word: "hater", scorePos: 1 },
+	{ word: "spout", scorePos: 1 },
+	{ word: "joist", scorePos: 1 },
+	{ word: "holly", scorePos: 1 },
+	{ word: "gayer", scorePos: 1 },
+	{ word: "foyer", scorePos: 1 },
+	{ word: "eager", scorePos: 1 },
+	{ word: "waver", scorePos: 1 },
+	{ word: "mover", scorePos: 1 },
+	{ word: "rover", scorePos: 1 },
+	{ word: "sweep", scorePos: 1 },
+	{ word: "skill", scorePos: 1 },
+	{ word: "gazer", scorePos: 1 },
+	{ word: "fever", scorePos: 1 },
+	{ word: "vaunt", scorePos: 1 },
+	{ word: "wafer", scorePos: 1 },
+	{ word: "mower", scorePos: 1 },
+	{ word: "rower", scorePos: 1 },
+	{ word: "homer", scorePos: 1 },
+	{ word: "jolly", scorePos: 1 },
+	{ word: "hover", scorePos: 1 },
+	{ word: "jaunt", scorePos: 1 },
+	{ word: "willy", scorePos: 1 },
+	{ word: "spell", scorePos: 1 },
+	{ word: "hilly", scorePos: 1 },
+	{ word: "swell", scorePos: 1 },
+	{ word: "refer", scorePos: 1 },
+	{ word: "smell", scorePos: 1 },
+	{ word: "fixer", scorePos: 1 },
+	{ word: "hazel", scorePos: 1 },
+	{ word: "joker", scorePos: 1 },
+	{ word: "kitty", scorePos: 1 },
+	{ word: "wound", scorePos: 1 },
+	{ word: "steam", scorePos: 1 },
+	{ word: "hound", scorePos: 1 },
+	{ word: "oaken", scorePos: 1 },
+	{ word: "awash", scorePos: 1 },
+	{ word: "puppy", scorePos: 1 },
+	{ word: "watch", scorePos: 1 },
+	{ word: "grown", scorePos: 1 },
+	{ word: "hatch", scorePos: 1 },
+	{ word: "husky", scorePos: 1 },
+	{ word: "puffy", scorePos: 1 },
+	{ word: "lunch", scorePos: 1 },
+	{ word: "witch", scorePos: 1 },
+	{ word: "hunch", scorePos: 1 },
+	{ word: "hitch", scorePos: 1 },
+	{ word: "udder", scorePos: 1 },
+	{ word: "wight", scorePos: 1 },
+	{ word: "gamma", scorePos: 1 },
+	{ word: "magma", scorePos: 1 },
+	{ word: "offer", scorePos: 1 },
+	{ word: "night", scorePos: 1 },
+]
 var GuessWords = require("./goodWords3.json").words
-const logging = false
+const logging = true
 function log(string) {
 	if (logging) console.log(string)
 }
@@ -165,16 +239,23 @@ function game(NewWord, wordIn) {
 			game(false, word)
 		}
 	} else {
-		var guess
+        var guess 
+        var guess2 = []
 		var guess1 = filterWords(goodSet, badSet, GuessWords)
 		var possibilities = filterWords(goodSet, badSet, Answers)
 		var filterGuesses = reduceGuess(goodSet, badSet)
+		if (turn == 1) {
+			for (let i = 0; i < guess1.length; i++) {
+				if (new Set(guess1[i].split("")).size == 5) {
+					guess2.push(guess1[i])
+				}
+            }
+            guess1 = guess2
+		}
+
 		if (possibilities.length <= 6 - turn) guess1 = possibilities
 		if (filterGuesses.length > 1 && Wordle.join("").length === 4 && possibilities.length > 6 - turn) guess1 = filterGuesses
-        if (filterGuesses.length > 1 && Wordle.join("").length === 4 && possibilities.length > 6 - turn) {
-            log(`one: ${(filterGuesses.length > 1).toString()} \ntwo: ${Wordle.join("").length === 4} \nthree: ${possibilities.length > 6 - turn}`)
-            process.exit(1)
-        }
+
 		if (usedWords.includes(guess1[0])) {
 			while (usedWords.includes(guess1[0])) {
 				guess1.shift()
@@ -214,16 +295,17 @@ function reduceGuess(badSet, goodSet) {
 	var specialLetters = new Set()
 	openWords.forEach((word) => {
 		specialLetters.add(word.split("")[openWordle])
-	})
-	log({ Wordle, openWords })
-	log(Wordle.join("").length)
+    })
+    
+
+    
 	GuessWords.forEach((word) => {
 		var i = 0
 		;[...new Set(word.word.split(""))].forEach((letter, index) => {
 			if (specialLetters.has(letter)) {
 				i++
 			}
-			if (i == 5) {
+			if (specialLetters.length - 1 == i || i == 5) {
 				onE.push(word)
 			}
 		})
